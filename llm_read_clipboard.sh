ecc717c4b1cc52c3858dfb71d2e230081e4cdb83
severinrichter <128835042+seve05@users.noreply.github.com>
2025-04-06 16:54:57 +0200

    Update llm_read_clipboard.sh
    .

--- llm_read_clipboard.sh
+++ llm_read_clipboard.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# use xclip if you're on X11, xsel or wayland. -b if you want to copy text into the clipboard and parse that to the model.
 PROMPT=$(xsel -o)
 gnome-terminal -- bash -c "ollama run gemma3:12b 'Give information about this: $PROMPT'; read -p 'Press Enter to close...'"
 
