96047fbe590fb95c51cd82de71ca5f755e574b15
severinrichter <128835042+seve05@users.noreply.github.com>
2025-04-06 23:51:07 +0200

    Add files via upload
    add image recognition

---
+++ image_rec.sh
@@ -0,0 +1,4 @@
+#bash -c executes the following 
+gnome-terminal --geometry=800x800 -- bash -c "python3 image_recognition.py; read -p 'Enter to close'"
+~                                                                                                             
+~                                                                                        
---
+++ image_recognition.py
@@ -0,0 +1,32 @@
+import subprocess
+import requests
+import io
+import base64
+from PIL import Image,ImageGrab
+
+# Load image buffer with PIL (optional)
+try:
+    image = ImageGrab.grabclipboard()
+except:
+    print("No Image found")
+    quit()
+#make iobytes object to write binary data
+buffered = io.BytesIO()
+image.save(buffered, format="PNG")
+image_bytes = buffered.getvalue()
+
+
+base64 = base64.b64encode(image_bytes)  #to base64
+base64_string = base64.decode('utf-8')   #to string
+
+
+# Send image and prompt to Ollama (assuming 'llava' model is running)
+response = requests.post("http://localhost:11434/api/generate", json={
+    "model": "gemma3:12b",
+    "prompt": "What's in this image, give information about the image like where its from, if possible.",
+    "images": [base64_string],
+    "stream": False
+})
+
+print(response.json()['response'])
+
