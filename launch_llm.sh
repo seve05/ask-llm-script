11541c5966df5bb1eb90bedefbe74be23abf4e45
severinrichter <128835042+seve05@users.noreply.github.com>
2025-04-10 14:07:14 +0200

    Delete launch_llm.sh
    del

--- launch_llm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#for the sake of completeness this is a super simple bash script i use with a shortcut
-gnome-terminal --geometry=800x800 -- bash -c "ollama run gemma3:12b; exec bash"        
